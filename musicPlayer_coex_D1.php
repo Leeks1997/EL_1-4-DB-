c729b925e3ac96ce5a1bfed175d4f8c4bdbaf404
kghyun <86757153+kghyun@users.noreply.github.com>
2021-07-23 15:08:05 +0900

    사용자 정보 조회(모두 조회, 랭킹)
     - cmd를 이용해 사용할 쿼리문 선별 가능

--- musicPlayer_coex_D1.php
+++ musicPlayer_coex_D1.php
@@ -3,8 +3,9 @@
 	$server_username = "team";
 	$server_password = "abcd1234";
 	$dbname = "coex";
-
-	$musicNum = $_POST["numPost"]; //검색할 곡 번호
+	
+	$cmd = $_POST["cmd"]; //실행할 쿼리 구문
+	$cmdNum = $_POST["numPost"]; //사용할 인자 값
 	
 	$conn = new mysqli($servername, $server_username, $server_password, $dbname);
 	
@@ -12,17 +13,33 @@
 	{
 		die("Connection Failed.". mysqli_connect_error());
 	}
-	//musicNum의 사용자 정보 조회
-	$sql = "select username, score from player WHERE musicnum = ".$musicNum." order by score desc"; 
-	$result = mysqli_query($conn, $sql);
-		
-	if(mysqli_num_rows($result) > 0){
-		while($row = mysqli_fetch_assoc($result)){
-			echo "|username:" .$row['username'];
-			echo "|score:" .$row['score'];
-			echo ";";
+
+	if($cmd == "all"){
+		//cmdNum의 사용자 정보 조회
+		$sql = "select username, score from player WHERE musicnum = ".$cmdNum." order by score desc"; 
+		$result = mysqli_query($conn, $sql);
+
+		if(mysqli_num_rows($result) > 0){
+			while($row = mysqli_fetch_assoc($result)){
+				echo "|username:" .$row['username'];
+				echo "|score:" .$row['score'];
+				echo ";";
+			}
+		}else {
+			echo "musiclevel not found";
 		}
-	}else {
-		echo "musiclevel not found";
+	}else if($cmd == "rank"){
+		//cmdNum의 랭킹
+		$sql = "select COUNT(*)+1 AS rank from player WHERE score >= ".$cmdNum; 
+		$result = mysqli_query($conn, $sql);
+
+		if(mysqli_num_rows($result) > 0){
+			while($row = mysqli_fetch_assoc($result)){
+				echo $row['rank'];
+			}
+		}else {
+			echo "rank not found";
+		}
 	}
+
 ?>
